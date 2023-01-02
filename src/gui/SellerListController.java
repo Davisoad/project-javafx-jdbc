3b6b0e62c9924094d8b8f71df98dc4323f57f064
Davisoad <davibragaz18@gmail.com>
2023-01-02 12:16:25 -0300

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -15,14 +16,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -62,12 +67,9 @@ public class SellerListController implements Initializable, DataChangeListener {
 
     @FXML
     public void onBtnNewAction(ActionEvent event) {
-        Stage parenStage = gui.util.Utils.currentStage(event);
+        Stage parentStage = Utils.currentStage(event);
         Seller obj = new Seller();
-        createDialogForm(obj, "/gui/SellerForm.fxml", parenStage);
-    }
-
-    private void createDialogForm(Seller obj, String string, Stage parenStage) {
+        createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
     }
 
     public void setSellerService(SellerService service) {
@@ -97,34 +99,34 @@ public class SellerListController implements Initializable, DataChangeListener {
             throw new IllegalStateException("Service was null");
         }
         List<Seller> list = service.findAll();
-        obsList = FXCollections.observableArrayList(list);
-        tableViewSeller.setItems(obsList);
-        initEditButtons();
-        initRemoveButtons();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewSeller.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
     }
 
-    // private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-    //     try {
-    //         FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-    //         Pane pane = loader.load();
+    private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
 
-    //         SellerFormController controller = loader.getController();
-    //         controller.setSeller(obj);
-    //         controller.setSellerService(new SellerService());
-    //         controller.subscribeDataChengeListener(this);
-    //         controller.updateFormData();
+            SellerFormController controller = loader.getController();
+            controller.setSeller(obj);
+            controller.setSellerService(new SellerService());
+            controller.subscribeDataChengeListener(this);
+            controller.updateFormData();
 
-    //         Stage dialoStage = new Stage();
-    //         dialoStage.setTitle("Enter Seller data");
-    //         dialoStage.setScene(new Scene(pane));
-    //         dialoStage.setResizable(false);
-    //         dialoStage.initOwner(parentStage);
-    //         dialoStage.initModality(Modality.WINDOW_MODAL);
-    //         dialoStage.showAndWait();
-    //     } catch (IOException e) {
-    //         Alerts.showAlert("IO Excepetion", "Error loading view", e.getMessage(), AlertType.ERROR);
-    //     }
-    // }
+            Stage dialoStage = new Stage();
+            dialoStage.setTitle("Enter Seller data");
+            dialoStage.setScene(new Scene(pane));
+            dialoStage.setResizable(false);
+            dialoStage.initOwner(parentStage);
+            dialoStage.initModality(Modality.WINDOW_MODAL);
+            dialoStage.showAndWait();
+        } catch (IOException e) {
+            Alerts.showAlert("IO Excepetion", "Error loading view", e.getMessage(), AlertType.ERROR);
+        }
+    }
 
     @Override
     public void onDataChanged() {
@@ -145,13 +147,8 @@ public class SellerListController implements Initializable, DataChangeListener {
                 }
                 setGraphic(button);
                 button.setOnAction(
-                        event -> createDialogForm(
-                                obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
-            }
-
-            private Object createDialogForm(Seller obj, String string, Stage currentStage) {
-                return null;
-            }
+                        event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+            }       
         });
     }
 
